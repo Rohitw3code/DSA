848d5a603cf8e4ce9d3aaec64bb20d42f935b060
bit-web24 <bitweb24@gamil.com>
2022-05-27 10:52:16 -0400

    circular linked list

---
+++ linkedlist/circularLL.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+struct node{
+	struct node* previous;
+	struct node* next;
+	int data;
+}
+
+
+int main(){
+	
+	struct node *head,*newnode, *temp;
+	int size = 5;
+
+
+	for (int i = 0; i < size; ++i)
+	{
+		newnode = (struct node*) malloc(sizeof(struct node));
+		printf("Enter a Data : ");
+		scanf("%d",&newnode->data);
+		newnode->next = 0;
+
+		if (head == 0)
+		{
+			head = newnode;
+			temp = newnode;
+		}
+		else{
+			temp->next = newnode;
+			temp = newnode;
+		}
+
+	}
+
+	temp.next = head;
+
+
+
+
+	return 0;
+}
